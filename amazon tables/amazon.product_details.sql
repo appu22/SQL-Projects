b5e2c3ffd9fcea2a94770b0df9c00815fe951958
appu22 <appu.xworkz@gmail.com>
2021-04-26 15:22:04 +0530

    group by,Aggregate Function

--- amazon tables/amazon.product_details.sql
+++ amazon tables/amazon.product_details.sql
@@ -9,9 +9,18 @@ USE AMAZON;
 DESC AMAZON.product_details;
 insert INTO amazon.product_details (P_ID , P_NAME,P_PRICE) VALUES (1,'MOBILE',15000.0);
 insert INTO amazon.product_details (P_ID , P_NAME,P_PRICE) VALUES (2,'MOBILE MI ',20000.0);
+insert INTO amazon.product_details (P_ID , P_NAME,P_PRICE) VALUES (5,'MOBILE MI ',6000.0);
+insert INTO amazon.product_details (P_ID , P_NAME,P_PRICE) VALUES (6,'MOBILE MI ',80000.0);
+insert INTO amazon.product_details (P_ID , P_NAME,P_PRICE) VALUES (7,'MOBILE MI ',80000.0);
 insert INTO amazon.product_details (P_ID , P_NAME,P_PRICE) VALUES (3,'MOBILE REALME',10000.0);
 insert INTO amazon.product_details (P_ID , P_NAME,P_PRICE) VALUES (4,'MOBILE SAM ' ,25000.0);
+--  group
+select count(P_ID), P_NAME from amazon.product_details group by P_NAME;
+select count(P_ID), P_PRICE from amazon.product_details group by P_PRICE;
 
+SELECT AVG(P_PRICE),P_NAME FROM amazon.product_details group by P_NAME;
 
+SELECT SUM(P_PRICE),P_NAME FROM amazon.product_details group by P_NAME HAVING sum(P_PRICE)>8000;
+SELECT COUNT(P_ID),P_NAME FROM amazon.product_details group by P_NAME HAVING COUNT(P_ID)>2 order by P_NAME;
 
 
